3fa1af958f367439079fec8f3e4b1e93a5ee091f
Weld Lucas Cunhas <cunha.lucas012@gmail.com>
2022-11-05 15:26:44 -0300

    Federated models training (1)

--- src/federated/federated_learning.py
+++ src/federated/federated_learning.py
@@ -7,21 +7,20 @@ from models.model import get_model
 
 
 def get_federated_avg_model(remote_models, train_set_sizes):
-    weights = OrderedDict()
+
     # compute the weighted average of the model weights/biases
+    weights = OrderedDict()
     for i in range(len(remote_models)):
         # save the remote trained model
         local_model_updates = remote_models[i].state_dict()
-
         if len(weights) == 0:
             for layer in local_model_updates.keys():
                 weights[layer] = local_model_updates[layer]*train_set_sizes[i]
         else:
             for layer in local_model_updates.keys():
-                weights[layer] = local_model_updates[layer]*train_set_sizes[i]
-
-    for layer in local_model_updates.keys():
-        weights[layer] = local_model_updates[layer] / sum(train_set_sizes)
+                weights[layer] += local_model_updates[layer]*train_set_sizes[i]
+    for layer in weights.keys():
+        weights[layer] = weights[layer] / sum(train_set_sizes)
 
     # create an empty model and load the combined weights to it
     combined_model = get_model()
